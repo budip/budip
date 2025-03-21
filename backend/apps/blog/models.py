0a90ba6abf4d42952e2578f00a17e957d4c085cb
Budi Prasetya <bpchen@gmail.com>
2025-03-21 11:23:03 -0700

    API: add basic blog using DRF

--- backend/apps/blog/models.py
+++ backend/apps/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
