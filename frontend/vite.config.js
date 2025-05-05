614ba06a216a091bc5385a046a8122bff125a5dd
Budi Prasetya <bpchen@gmail.com>
2025-05-05 14:09:03 -0700

    Core: fixing volume and arm64 errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: true,
     port: 5173,
+    watch: {
+      usePolling: true
+    },
     proxy: {
       '/api': {
         target: 'http://backend:8000',
-        changeOrigin: true,
-      },
-    },
-  },
+        changeOrigin: true
+      }
+    }
+  }
 })
